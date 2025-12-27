90e8e07ec6960b68a5308bcdf6544bafd6462296
loveright <2423623805@qq.com>
2025-12-27 13:02:55 +0800

    feat 项目初始化

---
+++ src/main/java/com/by/ai/langchain4j/ByAiApp.java
@@ -0,0 +1,11 @@
+package com.by.ai.langchain4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByAiApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ByAiApp.class, args);
+    }
+}
