49b775007bd1368deacac4fa0aee83280b357379
loveright <2423623805@qq.com>
2026-01-05 21:46:32 +0800

    feat 基于内存存储聊天记忆

--- src/main/java/com/by/ai/langchain4j/config/SeparateChatAssistantConfig.java
+++ src/main/java/com/by/ai/langchain4j/config/SeparateChatAssistantConfig.java
@@ -2,6 +2,7 @@ package com.by.ai.langchain4j.config;
 
 import dev.langchain4j.memory.chat.ChatMemoryProvider;
 import dev.langchain4j.memory.chat.MessageWindowChatMemory;
+import dev.langchain4j.store.memory.chat.InMemoryChatMemoryStore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,6 +18,10 @@ public class SeparateChatAssistantConfig {
 
     @Bean
     public ChatMemoryProvider chatMemoryProvider() {
-        return memoryId -> MessageWindowChatMemory.builder().id(memoryId).maxMessages(10).build();
+        return memoryId -> MessageWindowChatMemory
+                .builder()
+                .id(memoryId)
+                .chatMemoryStore(new InMemoryChatMemoryStore())
+                .maxMessages(10).build();
     }
 }
