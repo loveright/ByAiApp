7766353f59ca5a1866e7f6763a6ad5b9facfa4fe
loveright <2423623805@qq.com>
2026-01-11 14:02:15 +0800

    feat 测试向量转换和向量存储

--- src/test/java/com/by/ai/langchain4j/RAGTest.java
+++ src/test/java/com/by/ai/langchain4j/RAGTest.java
@@ -3,6 +3,9 @@ package com.by.ai.langchain4j;
 import dev.langchain4j.data.document.Document;
 import dev.langchain4j.data.document.loader.FileSystemDocumentLoader;
 import dev.langchain4j.data.document.parser.apache.pdfbox.ApachePdfBoxDocumentParser;
+import dev.langchain4j.data.segment.TextSegment;
+import dev.langchain4j.store.embedding.EmbeddingStoreIngestor;
+import dev.langchain4j.store.embedding.inmemory.InMemoryEmbeddingStore;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -34,5 +37,22 @@ public class RAGTest {
                 new ApachePdfBoxDocumentParser()
         );
         System.out.println(document);
+    }
+
+    @Test
+    public void testReadDocumentAndStore() {
+        //使用FileSystemDocumentLoader读取指定目录下的知识库文档
+        //并使用默认的文档解析器对文档进行解析(TextDocumentParser)
+        Document document = FileSystemDocumentLoader.loadDocument("src/main/resources/knowledge/人工智能.md");
+        //为了简单起见，我们暂时使用基于内存的向量存储
+        InMemoryEmbeddingStore<TextSegment> embeddingStore = new InMemoryEmbeddingStore<>();
+        //ingest
+        //1、分割文档：默认使用递归分割器，将文档分割为多个文本片段，每个片段包含不超过 300个token，并且有30个token的重叠部分保证连贯性
+        //DocumentByParagraphSplitter(DocumentByLineSplitter(DocumentBySentenceSplitter(DocumentByWordSplitter)))
+        //2、文本向量化：使用一个LangChain4j内置的轻量化向量模型对每个文本片段进行向量化
+        //3、将原始文本和向量存储到向量数据库中(InMemoryEmbeddingStore)
+        EmbeddingStoreIngestor.ingest(document, embeddingStore);
+        //查看向量数据库内容
+        System.out.println(embeddingStore);
     }
 }
